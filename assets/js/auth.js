b895d3f0bb6e3d3e56fab5e33d01dfd9751853ed
Zach Radford <zachradford13@gmail.com>
2017-06-15 20:32:43 -0700

    Added signout function to include a console log for clarity

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -40,7 +40,12 @@ var config = {
         
       } else {
         
-        firebase.auth().signOut();
+        // firebase.auth().signOut();
+        firebase.auth().signOut().then(function() {
+  			console.log('Signed Out');
+		}, function(error) {
+  			console.error('Sign Out Error', error);
+		});
         
       }
       
