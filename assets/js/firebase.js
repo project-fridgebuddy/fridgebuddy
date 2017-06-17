6d30813664f3ec36588f5af6ad59d8ef4556c8c1
ckingchris <christopherkingucla@gmail.com>
2017-06-17 11:00:13 -0700

    hide/show containers after search function

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -13,18 +13,16 @@ $(document).ready(function() {
 
   var database = firebase.database();
 
-  var fridge = [];
-
   var item = "";
 
   //Add items to firebase
 
-  $(".items").on('click', function(event) {
-
+  $(".item").on('click', function(event) {
+   
     // Keeps page from reloading //
     event.preventDefault();
     
-    item = $(this).val().trim().push(fridge);
+    item = this.innerText;
 
     database.ref().push({
       item: item
@@ -38,89 +36,64 @@ $(document).ready(function() {
 
     var item = snapshot.val().item;
 
-    var newRow = $('<tr>').attr('id', keyID);
-    newRow.append($('<td class="text-center">').text(item));
+    var newRow = $('<li>').attr('id', keyID);
+    newRow.append($('<p class="text-center">').text(item));
 
     $('#fridge-items').append(newRow);
 
   });
-
 });
 
 
-
-
-___________________________________
-//Adding Firebase authetication
-
-
-//Firebase config provided from Firebase
-var config = {
-    apiKey: "AIzaSyAJ84k3KTHl-zpSiDgr6SuaVmwIMxnDrsg",
-    authDomain: "fridgebuddy-da9f0.firebaseapp.com",
-    databaseURL: "https://fridgebuddy-da9f0.firebaseio.com",
-    projectId: "fridgebuddy-da9f0",
-    storageBucket: "fridgebuddy-da9f0.appspot.com",
-    messagingSenderId: "781721156427"
-  };
-
-  //Initialize Firebase application
-  firebase.initializeApp(config);
-
-
-    
-    // Function for login
+//------------OAuth---------------//
+//Function called for login/out button listener
     function toggleSignIn() {
       if (!firebase.auth().currentUser) {
-       
         var provider = new firebase.auth.GoogleAuthProvider();
-        //'profile' referring to that data our application has access to ie 'Basic profile email, name, picture'
+        
         provider.addScope('profile');
-       
         firebase.auth().signInWithPopup(provider).then(function(result) {
-          // This gives you a Google Access Token. You can use it to access the Google API.
+          //Gives Google access token
           var token = result.credential.accessToken;
-          // The signed-in user info.
+        
           var user = result.user;
-          document.getElementById('quickstart-oauthtoken').textContent = token;
 
         }).catch(function(error) {
-          //Error handler for clarity
+          //REcords errors for clarity
           var errorCode = error.code;
-
           var errorMessage = error.message;
-          // User email address used when attempting to login
-
           var email = error.email;
-         
           var credential = error.credential;
-          
           if (errorCode === 'auth/account-exists-with-different-credential') {
             alert('You have already signed up with a different auth provider for that email.');
             
           } else {
             console.error(error);
           }
-          
+        
         });
         
       } else {
         
-        firebase.auth().signOut();
+        // firebase.auth().signOut();
+        firebase.auth().signOut().then(function() {
+        console.log('Signed Out');
+    }, function(error) {
+        console.error('Sign Out Error', error);
+    });
         
       }
       
-      document.getElementById('quickstart-sign-in').disabled = true;
+      document.getElementById('login').disabled = true;
       
     }
     
-
-    //Setup Ui listeners and updates
+    //Called on page/window load to add listener to UI
     function initApp() {
-      // Listening for state changes.
+      //Listens for state changes
       firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-          // User is signed in.
+          
           var displayName = user.displayName;
           var email = user.email;
           var emailVerified = user.emailVerified;
@@ -128,30 +101,22 @@ var config = {
           var isAnonymous = user.isAnonymous;
           var uid = user.uid;
           var providerData = user.providerData;
-          
-          document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-          document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-          document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+         
+          document.getElementById('login').textContent = 'Sign out';
           
         } else {
-          // User is signed out.
-          document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-          document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
-          document.getElementById('quickstart-account-details').textContent = 'null';
-          document.getElementById('quickstart-oauthtoken').textContent = 'null';
+          
+          document.getElementById('login').textContent = 'Sign in with Google';
+       
         }
-        document.getElementById('quickstart-sign-in').disabled = false;
+        
+        document.getElementById('login').disabled = false;
+        
       });
 
-      document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+      document.getElementById('login').addEventListener('click', toggleSignIn, false);
     }
 
-    //Function to call initialize application on window/document load
     window.onload = function() {
       initApp();
     };
-
-
-    //Button for sign in with ID
-
-    // <button disabled class="mdl-button mdl-js-button mdl-button--raised" id="quickstart-sign-in">Sign in with Google</button>
